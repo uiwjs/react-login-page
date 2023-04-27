5d245f4d0fb6a20582162689a93f6ec5dae4cf8a
jaywcjlove <398188662@qq.com>
2023-04-27 14:28:40 +0800

    type: fix button type errors.

--- pages/base/src/control/Reset.tsx
+++ pages/base/src/control/Reset.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Button, ButtonProps } from 'react-login-page';
 
-export const Reset: FC<Omit<ButtonProps, 'name'>> = (props) => {
+export const Reset: FC<ButtonProps> = (props) => {
   const { ...elmProps } = props;
   if (!elmProps.children) {
     elmProps.children = 'Reset';
--- pages/page4/src/control/Submit.tsx
+++ pages/page4/src/control/Submit.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Button, ButtonProps } from 'react-login-page';
 
-export const Submit: FC<Omit<ButtonProps, 'name'>> = (props) => {
+export const Submit: FC<ButtonProps> = (props) => {
   const { ...elmProps } = props;
   if (!elmProps.children) {
     elmProps.children = <div>Log in</div>;
